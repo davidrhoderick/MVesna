e62246306bc08ec3c6129db65bb12e2ee0c79199
David Rhoderick <david@artlyticalmedia.com>
2019-07-20 17:08:25 +0200

    Initial commit

---
+++ wp-content/themes/mvesna-theme/gulpfile.js
@@ -0,0 +1,46 @@
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    include     = require('gulp-include'),
+    browserSync = require('browser-sync').create(),
+    jshint      = require('gulp-jshint'),
+    minify      = require('gulp-minify'),
+    sassLint    = require('gulp-sass-lint'),
+    notify      = require('gulp-notify');
+
+gulp.task('sass', function(){
+  return gulp.src('static/scss/style.scss')
+    .pipe(sass())
+      .on('error', notify)
+    .pipe(sassLint({
+      files: { ignore: 'static/bower_components/**/*.scss' }
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', function(){
+  return gulp.src('static/js/site.js')
+    .pipe(include())
+      .on('error', notify)
+    .pipe(jshint.reporter('default'))
+    .pipe(minify({
+      ext:{
+        src: '.js',
+        min: '.min.js'
+      }
+    }))
+    .pipe(gulp.dest('static'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['sass', 'js'], function () {
+  browserSync.init({
+    proxy: 'http://mvesna.test'
+  });
+
+  gulp.watch('static/scss/**/*.scss', ['sass']);
+  gulp.watch('static/js/**/*.js', ['js']);
+  gulp.watch(['*.php', 'templates/**/*.twig']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
